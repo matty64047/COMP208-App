f6cb77b6b6e634439f73f3ebb6148490bf0ee0bc
matty64047 <54942633+matty64047@users.noreply.github.com>
2021-02-28 13:10:51 +0000

    Update server.py
    made the authentication function and started working on communicating with the database

--- Back End/server.py
+++ Back End/server.py
@@ -1,13 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 import mysql.connector
-import requests
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="",
-  password="",
-  database=""
+  user="root",
+  password="matthew123",
+  database="comp208"
 )
 
 app = Flask(__name__)
@@ -32,8 +31,40 @@ def get_favourites():
 def new_user():
     return "Not Implemented Yet"
 
-def authenticate_user(user_id, security_key):
-    return True
+@app.route('/get_user', methods=['POST'])
+def get_user():
+    user_id = request.form.get('user_id')
+    password = request.form.get('password')
+    valid = authenticate_user(user_id, password)
+    if valid == True:
+        user = sql_query("select * from Users where UserID=%s;" % (user_id))
+        user_dict = {
+            "user_id" : user[0],
+            "last_name" : user[1],
+            "first_name" : user[2],
+            "address" : user[3],
+            "university" : user[4],
+            "email" : user[5],
+            "password" : user[6],
+        }
+        return "True"
+    else: 
+        return valid
+
+def authenticate_user(user_id, password):
+    response = sql_query("select Password from Users where UserID=%s;" % (user_id))
+    if response:
+        if response[0] == password:
+            return True 
+        else:
+            return "Incorrect Password"
+    else: 
+        return "User does not exist"
+
+def sql_query(query):
+    mycursor = mydb.cursor()
+    mycursor.execute(query)
+    return mycursor.fetchone()
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug=True)
